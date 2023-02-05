bb99040442f6fdc5c0a97195816ef2f36c8f901c
apt-ttevis <tyler.m.tevis@gmail.com>
2023-02-04 22:47:50 -0500

    Initial Commit

---
+++ command.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+process = "OfficialChatGPT --api_key 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 --stream"
+parameter = "What is love, baby don't hurt me... Don't hurt me... No more.\n\n"
+os.system(process)
+# time.sleep(3)
+input = input("Prompt: ")
